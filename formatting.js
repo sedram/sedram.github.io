66cc634d7c43963ca55286459c9aea3bb606cf0b
sedram <54665381+sedram@users.noreply.github.com>
2024-09-29 15:41:27 -0400

    Added more links and got Dark mode settings working on Firefox

--- formatting.js
+++ formatting.js
@@ -1,10 +1,17 @@
-const mainSingleton = document.getElementsByTagName("main")
+// set up links:
+const mainSingleton = document.getElementsByTagName("main");
 for (const main of mainSingleton) {
-    const links = main.getElementsByTagName("a")
-    for (const link of links) {
-        link.setAttribute("target","_blank")
-        link.setAttribute("rel","noopener noreferrer")
-        link.setAttribute("class","underline text-blue-600 hover:text-blue-800 visited:text-purple-600")
-        // TODO: FIXME: Expand the above to work for dark mode
-    }
+  const links = main.getElementsByTagName("a");
+  for (const link of links) {
+    link.setAttribute("target", "_blank");
+    link.setAttribute("rel", "noopener noreferrer");
+    link.setAttribute(
+      "class",
+      "underline text-blue-600 hover:text-blue-800 visited:text-purple-600 dark:text-sky-600 dark:hover:text-sky-400 dark:visited:text-indigo-500"
+    );
+    // https://nearest-tailwind-color.netlify.app/ gave me dark colors which are used on https://docs.python.org/3/
+  }
 }
+// TODO: FIXME: Add a toggle switch to turn informational links off (by storing the link in the id, for instance), unless they're within something whose class is downloads.
+
+//TODO:Could use https://youtu.be/WTchW0LdWL0 approach to define a master data-theme for dark mode using CSS variables
